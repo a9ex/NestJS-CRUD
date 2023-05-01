3ad3bc783b282106fb6915815a4b6e6b7dd74aaa
Alexandre BARBERIS <alexandre.barberis@epitech.eu>
2023-05-01 23:33:53 +0200

    feat: add JWT strategy

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      secretOrKey: configService.get('JWT_SECRET'),
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: any) {
+    return { id: payload.id };
+  }
+}
